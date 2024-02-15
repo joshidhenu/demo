6a0d7ab40f92eaf6ecf990037f299841caa25eab
joshidhenu <dhenujoshi227@gmail.com>
2024-02-15 12:04:13 +0530

    first commit

---
+++ src/main/java/com/demo/update/dao/UpdateDao.java
@@ -0,0 +1,8 @@
+package com.demo.update.dao;
+
+public class UpdateDao {
+
+	private String jdbcURL = "jdbc:mysql://localhost:3306/studentmanagement_db?useSSL=false";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "root";
+}
---
+++ src/main/java/com/demo/update/model/Update.java
@@ -0,0 +1,8 @@
+package com.demo.update.model;
+
+public class Update {
+	
+	private int id;
+	private String name;
+	private String email;
+}
